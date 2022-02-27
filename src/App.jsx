b7380007c6c7fe66531653aab7ffacac10160135
EduCsg <eduardocasagrande71@gmail.com>
2022-02-26 23:21:29 -0300

    Timer configuration complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const COUNTDOWN_INITIAL_TIME_IN_SECONDS = 60 * 25; //25 minutes
 
 function App() {
+	const [secondsAmount, setSecondsAmount] = useState(
+		COUNTDOWN_INITIAL_TIME_IN_SECONDS,
+	);
+
+	const minutes = Math.floor(secondsAmount / 60);
+	const seconds = Math.floor(secondsAmount % 60);
+
+	useEffect(() => {
+		if (secondsAmount > 0) {
+			setTimeout(() => {
+				setSecondsAmount((state) => state - 1);
+			}, 1000);
+		} else {
+			alert("Chegou ao fim!");
+		}
+	}, [secondsAmount]);
+
 	return (
 		<div>
-			<span>00</span>
+			<span>{String(minutes).padStart(2, "0")}</span>
 			<span>:</span>
-			<span>00</span>
+			<span>{String(seconds).padStart(2, "0")}</span>
 		</div>
 	);
 }
