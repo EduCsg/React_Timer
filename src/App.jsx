f9227dd549b3e809faaa83472aafb05ed6be3368
EduCsg <eduardocasagrande71@gmail.com>
2022-02-28 17:40:51 -0300

    Adds a simple style to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "./styles.css";
 
 const COUNTDOWN_INITIAL_TIME_IN_SECONDS = 60 * 25; //25 minutes
 
@@ -21,10 +22,13 @@ function App() {
 	}, [secondsAmount]);
 
 	return (
-		<div>
-			<span>{String(minutes).padStart(2, "0")}</span>
-			<span>:</span>
-			<span>{String(seconds).padStart(2, "0")}</span>
+		<div className="div-container">
+			<h1 className="main-title">Timer Counter</h1>
+			<div className="span-wrapper">
+				<span className="span-1">{String(minutes).padStart(2, "0")}</span>
+				<span className="span-2">:</span>
+				<span className="span-3">{String(seconds).padStart(2, "0")}</span>
+			</div>
 		</div>
 	);
 }
